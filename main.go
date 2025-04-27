572b5f8386caf77bd7066c76669b9b61fe870872
Karina <karina.isl@inbox.ru>
2025-04-27 21:47:45 +0900

    matrix

--- main.go
+++ main.go
@@ -6,16 +6,36 @@ import (
 )
 
 func main(){
-	messages := [3]string{"1", "2", "3"}
-	messages[2] = "5"
+	 //messages := make([] string, 10000) 
 	
-	printMessages(messages)
+	
+	//printMessages(messages)
+	//fmt.Println(len(messages))
+	//fmt.Println(cap(messages))
+	//fmt.Println(messages)
+	//messages = append(messages, "10001")
+	//fmt.Println(len(messages))
+	//fmt.Println(cap(messages))
+	matrix := make([][] int, 10)
+	counter := 0
+	for x := 0; x < 10; x++{
+		matrix[x] = make([] int, 10)
+		for y := 0; y < 10; y++{
+			counter++
+			
+			matrix[x][y] = counter
+		}
+		fmt.Println(matrix[x])
+	}
+
 }
 func printMessages(messages [3] string) error{
 	if len(messages) == 0{
 			return errors.New("Ошибка")
 	}
-   fmt.Println(messages)
+
+
+   
 
 	return nil
 }
